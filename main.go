e260fc34e2bf759db23e118b19601fa526638705
kongwoojin <kongwoojin03@gmail.com>
2023-06-19 09:34:14 +0900

    feat: Remove unused routes

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"koreatech-board-api/routes"
-	"net/http"
 )
 
 func main() {
@@ -14,17 +13,9 @@ func main() {
 	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-
-	// Routes
-	e.GET("/", hello)
-
+	
 	routes.APIRouter(e)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
-}
-
-// Handler
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
 }
